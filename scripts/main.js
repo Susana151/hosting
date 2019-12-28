49bf463282c6b31f4d46335cfbaf2c1b7c14e8dd
Susana151 <58435859+Susana151@users.noreply.github.com>
2019-12-28 20:47:36 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,53 +1,69 @@
+
 // Image switcher code
 
 let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
-  let mySrc = myImage.getAttribute('src');
-  if(mySrc === 'images/wooden-spoon-4546584_960_720.png.png') {
-    myImage.setAttribute ('src','images/tortilla-622974_960_720.png');
-  } else {
-    myImage.setAttribute ('src','images/wooden-spoon-4546584_960_720.png.png');
-  }
+ let mySrc = myImage.getAttribute('src');
+ if(mySrc === 'images/present-1893642_1920.jpg') {
+
+   myImage.setAttribute ('src','images/access-2676041_1920.jpg');
+
+
+ } else {
+   myImage.setAttribute ('src','images/present-1893642_1920.jpg');
+ }
 }
 
+
 // Personalized welcome message code
+
 
 let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
 
+
 function setUserName() {
-  let myName = prompt('Inserta tu apodo. El que más te guste. ;-)');
-  if(!myName || myName === null) {
-    
-  } else {
-    localStorage.setItem('name', myName);
-    myHeading.innerHTML = 'Bienvenido a MiVä´s World, ' + myName + '. ¡Disfruta!';
-  }
+ let myName = prompt('Inserta tu nombre.');
+
+  
+ if(!myName || myName === null) {
+ } else {
+   localStorage.setItem('name', myName);
+   myHeading.innerHTML = '¡Feliz cumpleaños' + myName + '!';
+ }
 }
 
+
 if(!localStorage.getItem('name')) {
-  setUserName();
+ setUserName();
 } else {
-  let storedName = localStorage.getItem('name');
-  myHeading.innerHTML = 'Bienvenido a MiVä´s World, ' + storedName + '. ¡Disfruta!';
+ let storedName = localStorage.getItem('name');
+ myHeading.innerHTML = '¡Feliz cumpleaños ' + storedName + '!';
 }
 
 myButton.onclick = function() {
-  setUserName();
+ setUserName();
 }
 
-// El gato desplazándose <3
-var catEl = document.getElementById("cat");
 
-var startTime = new Date().getTime();    
+
+
+// El gato desplazándose <3
+
+var catEl = document.getElementById("cat");
+var startTime = new Date().getTime();   
 var walkTheCat = function(){
-    var currTime = new Date().getTime();
-    var newLeft = ((currTime - startTime) / 1000) * 100;
-    var newTop = 0//((currTime - startTime) / 1000) * 100;
-    if (newLeft > 850){ startTime = currTime};
-    catEl.style.left = newLeft + "px";
-    catEl.style.top = newTop + "px";
-    window.requestAnimationFrame(walkTheCat);
+   var currTime = new Date().getTime();
+   var newLeft = ((currTime - startTime) / 1000) * 100;
+   var newTop = 0//((currTime - startTime) / 1000) * 100;
+   if (newLeft > 850){ startTime = currTime};
+   catEl.style.left = newLeft + "px";
+   catEl.style.top = newTop + "px";
+   window.requestAnimationFrame(walkTheCat);
+
+
 };
+
+
 walkTheCat();
